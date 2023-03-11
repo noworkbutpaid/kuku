ff1d76676bf25bce4af4e267bea62ed54f70d46f
Regetta <dprequim@gmail.com>
2023-03-12 06:42:43 +0800

    initial commit

---
+++ index7b17.php
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom" xml:lang="en-gb">
+	<title type="text">COCO CEO</title>
+	<subtitle type="text">Get ready to join the ultimate pack of dog-loving tech enthusiasts with COCO CEO! Our community-driven token is all about bringing together the best of meme culture and crypto innovation. With features like staking, farming, and NFTs, we're building a project that's as fun as it is useful. So come join our pack and let's unleash the power of the COCO CEO community together - after all, we're all just a group of people who share a passion for pups and progress!</subtitle>
+	<link rel="alternate" type="text/html" href="https://cococeo.com"/>
+	<id>https://cococeo.com/index.php</id>
+	<updated>2023-03-09T01:47:08+00:00</updated>
+	<generator uri="https://www.joomla.org">Joomla! - Open Source Content Management</generator>
+	<link rel="self" type="application/atom+xml" href="https://cococeo.com/index.php?format=feed&amp;type=atom"/>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>COCO CEO</title>
+		<description><![CDATA[Get ready to join the ultimate pack of dog-loving tech enthusiasts with COCO CEO! Our community-driven token is all about bringing together the best of meme culture and crypto innovation. With features like staking, farming, and NFTs, we're building a project that's as fun as it is useful. So come join our pack and let's unleash the power of the COCO CEO community together - after all, we're all just a group of people who share a passion for pups and progress!]]></description>
+		<link>https://cococeo.com/index.php</link>
+		<lastBuildDate>Thu, 09 Mar 2023 01:47:07 +0000</lastBuildDate>
+		<atom:link rel="self" type="application/rss+xml" href="https://cococeo.com/index.php?format=feed&amp;type=rss"/>
+		<language>en-gb</language>
+	</channel>
+</rss>
